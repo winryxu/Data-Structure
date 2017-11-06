2547586b67c5815db1133b1f639402af1753d1e7
winryxu <33185560+winryxu@users.noreply.github.com>
2017-11-06 00:46:32 -0600

    Add files via upload

---
+++ mp4/gradientColorPicker.cpp
@@ -0,0 +1,73 @@
+#include <stdlib.h>
+#include "gradientColorPicker.h"
+
+/**
+ * Constructs a new gradientColorPicker.
+ *
+ * @param fadeColor1 The first color to start the gradient at.
+ * @param fadeColor2 The second color to end the gradient with.
+ * @param radius How quickly to transition to fadeColor2.
+ * @param centerX X coordinate for the center of the gradient.
+ * @param centerY Y coordinate for the center of the gradient.
+ */
+gradientColorPicker::gradientColorPicker(RGBAPixel fadeColor1,
+                                         RGBAPixel fadeColor2, int radius,
+                                         int centerX, int centerY)
+{
+    /**
+     * @todo Construct your gradientColorPicker here! You may find it
+     *	helpful to create additional member variables to store things.
+     */
+    c1 = fadeColor1;
+    c2 = fadeColor2;
+    r = radius;
+    xcoor = centerX;
+    ycoor = centerY;
+}
+
+/**
+ * Picks the color for pixel (x, y).
+ *
+ * The first color fades into the second color as you move from the initial
+ * fill point, the center, to the radius. Beyond the radius, all pixels
+ * should be just color2.
+ *
+ * You should calculate the distance between two points using the standard
+ * Manhattan distance formula,
+ *
+ * \f$d = |center\_x - given\_x| + |center\_y - given\_y|\f$
+ *
+ * Then, scale each of the three channels (R, G, and B) from fadeColor1 to
+ * fadeColor2 linearly from d = 0 to d = radius.
+ *
+ * For example, the red color at distance d where d is less than the radius
+ * must be
+ *
+ * \f$ redFill = fadeColor1.red - \left\lfloor
+   \frac{d*fadeColor1.red}{radius}\right\rfloor +
+   \left\lfloor\frac{d*fadeColor2.red}{radius}\right\rfloor\f$
+ *
+ * Note that all values are integers. If you do not follow this formula,
+ * your colors may be very close but round differently than ours.
+ *
+ * @param x The x coordinate to pick a color for.
+ * @param y The y coordinate to pick a color for.
+ * @return The color selected for (x, y).
+ */
+RGBAPixel gradientColorPicker::operator()(int x, int y)
+{
+    RGBAPixel color;
+    int d = abs(xcoor - x) + abs(ycoor - y);
+    if (d < r)
+    {
+      color.red = c1.red - (d * c1.red / r) + (d * c2.red / r);
+      color.green = c1.green - (d * c1.green / r) + (d * c2.green / r);
+      color.blue = c1.blue - (d * c1.blue / r) + (d * c2.blue / r);
+      return color;
+    }
+    else return c2;
+    /**
+     * @todo Return the correct color here!
+     */
+
+}
